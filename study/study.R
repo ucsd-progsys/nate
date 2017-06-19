286a45b8f4ad369dbd382e1e8ec783c3d265e1aa
Eric Seidel <eric@seidel.io>
2017-06-19 14:57:33 -0700

    add other user-study data

--- study/study.R
+++ study/study.R
@@ -2,8 +2,10 @@ library(irr)
 
 a0 = read.csv(file="study-data/Types_Study_A_scores.csv")
 b0 = read.csv(file="study-data/Types_Study_B_scores.csv")
-
-reasons0 = c(a0)
+a1 = read.csv(file="study-data/Types_Study_1_A_scores.csv")
+b1 = read.csv(file="study-data/Types_Study_1_B_scores.csv")
+a2 = read.csv(file="study-data/Types_Study_2_A_scores.csv")
+b2 = read.csv(file="study-data/Types_Study_2_B_scores.csv")
 
 reasons0 = c(a0$X1..sepConcat.explain..1.0.pts.,
              a0$X3..padZero.explain..1.0.pts.,
@@ -11,10 +13,22 @@ reasons0 = c(a0$X1..sepConcat.explain..1.0.pts.,
              b0$X1..sepConcat.explain..1.0.pts.,
              b0$X3..padZero.explain..1.0.pts.,
              b0$X5..mulByDigit.explain..1.0.pts.)
-#reasons = data.frame(reasons0)
-## filter out answers that any annotator marked as INCOMPLETE
-# reasons = reasons[reasons$reasons0 != -1 & reasons$reasons1 != -1 & reasons$reasons2 != -1 & reasons$reasons3 != -1,]
-#reasons = reasons[reasons$reasons0 != -1]
+reasons1 = c(a1$X1..sepConcat.explain..1.0.pts.,
+             a1$X3..padZero.explain..1.0.pts.,
+             a1$X5..mulByDigit.explain..1.0.pts.,
+             b1$X1..sepConcat.explain..1.0.pts.,
+             b1$X3..padZero.explain..1.0.pts.,
+             b1$X5..mulByDigit.explain..1.0.pts.)
+reasons2 = c(a2$X1..sepConcat.explain..1.0.pts.,
+             a2$X3..padZero.explain..1.0.pts.,
+             a2$X5..mulByDigit.explain..1.0.pts.,
+             b2$X1..sepConcat.explain..1.0.pts.,
+             b2$X3..padZero.explain..1.0.pts.,
+             b2$X5..mulByDigit.explain..1.0.pts.)
+reasons = data.frame(reasons0, reasons1, reasons2)
+# filter out answers that any annotator marked as INCOMPLETE
+reasons = reasons[reasons$reasons0 != -1 & reasons$reasons1 != -1 & reasons$reasons2 != -1,]
+
 
 fixes0 = c(a0$X2..sepConcat.fix..1.0.pts.,
            a0$X4..padZero.fix..1.0.pts.,
@@ -22,25 +36,35 @@ fixes0 = c(a0$X2..sepConcat.fix..1.0.pts.,
            b0$X2..sepConcat.fix..1.0.pts.,
            b0$X4..padZero.fix..1.0.pts.,
            b0$X6..mulByDigit.fix..1.0.pts.)
-#fixes = data.frame(fixes0)
-## filter out answers that any annotator marked as INCOMPLETE
-#fixes = fixes[fixes$fixes0 != -1]
+fixes1 = c(a1$X2..sepConcat.fix..1.0.pts.,
+           a1$X4..padZero.fix..1.0.pts.,
+           a1$X6..mulByDigit.fix..1.0.pts.,
+           b1$X2..sepConcat.fix..1.0.pts.,
+           b1$X4..padZero.fix..1.0.pts.,
+           b1$X6..mulByDigit.fix..1.0.pts.)
+fixes2 = c(a2$X2..sepConcat.fix..1.0.pts.,
+           a2$X4..padZero.fix..1.0.pts.,
+           a2$X6..mulByDigit.fix..1.0.pts.,
+           b2$X2..sepConcat.fix..1.0.pts.,
+           b2$X4..padZero.fix..1.0.pts.,
+           b2$X6..mulByDigit.fix..1.0.pts.)
+fixes = data.frame(fixes0, fixes1, fixes2)
+# filter out answers that any annotator marked as INCOMPLETE
+fixes = fixes[fixes$fixes0 != -1 & fixes$fixes1 != -1 & fixes$fixes2 != -1,]
 
-# print("REASONS")
-# print(table(reasons[,1]))
-# print(table(reasons[,2]))
-# print(table(reasons[,3]))
-# print(table(reasons[,4]))
-# print(kappam.fleiss(reasons,exact=TRUE,detail=TRUE))
-# #print(kappam.fleiss(reasons[1:3],detail=TRUE))
+print("REASONS")
+print(table(reasons[,1]))
+print(table(reasons[,2]))
+print(table(reasons[,3]))
+print(kappam.fleiss(reasons[,1:3],exact=TRUE,detail=TRUE))
+#print(kappam.fleiss(reasons[1:3],detail=TRUE))
 
-# print("FIXES")
-# print(table(fixes[,1]))
-# print(table(fixes[,2]))
-# print(table(fixes[,3]))
-# print(table(fixes[,4]))
-# print(kappam.fleiss(fixes,exact=TRUE,detail=TRUE))
-# #print(kappam.fleiss(fixes[1:3],detail=TRUE))
+print("FIXES")
+print(table(fixes[,1]))
+print(table(fixes[,2]))
+print(table(fixes[,3]))
+print(kappam.fleiss(fixes[,1:3],exact=TRUE,detail=TRUE))
+#print(kappam.fleiss(fixes[1:3],detail=TRUE))
 
 significance <- function(x,y) {
   print(x)
